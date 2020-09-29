54fc7c15751e2e8db1368eb05c793c95d690b44e
Andrei <aikey@mail.ru>
2020-09-29 03:13:32 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  else {
+    return Math.min(...array);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {return 0;}
+  else { return Math.max(...array); }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {return 0}
+  else {let average = 0;
+  for (let i = 0; i < array.length; i +=1){
+    average += array[i];
+  }
+  return average / array.length;}
 }
